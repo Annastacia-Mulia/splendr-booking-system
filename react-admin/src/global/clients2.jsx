1bcf498469b250ae1c1028a911002ab5b6e31794
John Doe <annastacia.nzoka@strathmore.edu>
2024-07-03 23:02:40 +0300

    client reading from database

---
+++ react-admin/src/global/clients2.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import ClientsList from './clients';
+
+function Clients2 () {
+    return (
+      <div>
+        <ClientsList />
+      </div>
+    )
+  }
+export default Clients2;
