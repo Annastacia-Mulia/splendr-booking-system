2a26822fd39a6401a5d17638614e64142ff9cbe1
Shikulisa <lisa.wanjiku@strathmoree.com>
2024-07-03 14:42:18 +0300

    Client Home screen

---
+++ splendrApp/App/Screens/ClientHomeScreen.js
@@ -0,0 +1,237 @@
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, SafeAreaView, View, Text, TouchableOpacity, ScrollView, Dimensions, FlatList } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import axios from 'axios';
+import LogoutConfirmation from './LogoutConfirmation'; // Import the modal component
+import Config from './config'; // Import the config file
+
+const { width } = Dimensions.get('window');
+
+const ClientHomeScreen = () => {
+  const navigation = useNavigation();
+  const [logoutModalVisible, setLogoutModalVisible] = useState(false);
+  const [businesses, setBusinesses] = useState([]);
+  const [selectedService, setSelectedService] = useState('');
+
+  useEffect(() => {
+    if (selectedService) {
+      axios.get(`${Config.API_URL}/api/businesses?service=${selectedService}`)
+        .then(response => {
+          console.log('Fetched businesses:', response.data); // Log the response data
+          navigation.navigate('BusinessListScreen', { businesses: response.data });
+        })
+        .catch(error => {
+          if (error.response) {
+            console.error('Error fetching businesses (response):', error.response.data);
+          } else if (error.request) {
+            console.error('Error fetching businesses (request):', error.request);
+          } else {
+            console.error('Error fetching businesses (message):', error.message);
+          }
+        });
+    }
+  }, [selectedService]);
+
+  const handleLogout = () => {
+    setLogoutModalVisible(true);
+  };
+
+  const handlePressService = (service) => {
+    setSelectedService(service);
+  };
+
+  const confirmLogout = () => {
+    // Perform logout logic here (clear user data, tokens, etc.)
+    // For example, navigate to Login screen after logout
+    navigation.navigate('Login');
+  };
+
+  const cancelLogout = () => {
+    setLogoutModalVisible(false);
+  };
+
+  const renderBusinessItem = ({ item }) => (
+    <View style={styles.businessItem}>
+      <Text style={styles.businessName}>{item.name}</Text>
+      <Text style={styles.businessDetails}>{item.address}</Text>
+      <Text style={styles.businessDetails}>{item.phone}</Text>
+    </View>
+  );
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.title}>Welcome to Splendr</Text>
+        <Text style={styles.subtitle}>The Ultimate Beauty Booking Application</Text>
+      </View>
+
+      
+      <ScrollView contentContainerStyle={styles.scrollContainer}>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.boxButton} onPress={() => handlePressService('Hair')}>
+            <Text style={styles.boxButtonText}>Hair</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.boxButton} onPress={() => handlePressService('Barbering')}>
+            <Text style={styles.boxButtonText}>Barbering</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.boxButton} onPress={() => handlePressService('Nails')}>
+            <Text style={styles.boxButtonText}>Nails</Text>
+          </TouchableOpacity>
+        </View>
+
+        {selectedService && (
+          <FlatList
+            data={businesses}
+            keyExtractor={item => (item.id ? item.id.toString() : Math.random().toString())} // Add check for item.id
+            renderItem={renderBusinessItem}
+            contentContainerStyle={styles.businessList}
+          />
+        )}
+      </ScrollView>
+
+      
+
+      <View style={styles.footer}>
+        <TouchableOpacity style={styles.footerIcon} onPress={() => navigation.navigate('ClientHomeScreen')}>
+          <Icon name="home" size={24} color="#075eec" />
+          <Text style={styles.footerIconText}>Home</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.footerIcon} onPress={() => navigation.navigate('AppointmentBookingScreen')}>
+          <Icon name="calendar" size={24} color="#075eec" />
+          <Text style={styles.footerIconText}>Appointments</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.footerIcon} onPress={() => navigation.navigate('ClientProfile')}>
+          <Icon name="user" size={24} color="#075eec" />
+          <Text style={styles.footerIconText}>Profile</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default ClientHomeScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#e8ecf4',
+  },
+  header: {
+    alignItems: 'center',
+    marginVertical: 36,
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: '700',
+    color: '#075eec',
+    marginBottom: 6,
+  },
+  subtitle: {
+    fontSize: 18,
+    fontWeight: '500',
+    color: '#929292',
+  },
+  profileIcon: {
+    position: 'absolute',
+    top: 10,
+    right: 10,
+  },
+  scrollContainer: {
+    flexGrow: 1,
+    paddingHorizontal: 24,
+    paddingBottom: 20,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+  },
+  boxButton: {
+    width: width * 0.85,
+    height: width * 0.40,
+    backgroundColor: '#fff',
+    padding: 10,
+    borderRadius: 10,
+    marginVertical: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    elevation: 2,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+  },
+  boxButtonText: {
+    fontSize: 16,
+    fontWeight: '600',
+    color: '#1D2A32',
+    textAlign: 'center',
+    flex: 1,
+  },
+  content: {
+    alignItems: 'center',
+    paddingBottom: 20,
+  },
+  button: {
+    backgroundColor: '#075eec',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 25,
+    marginVertical: 10,
+  },
+  profileButton: {
+    backgroundColor: '#075eec',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 25,
+    marginVertical: 10,
+  },
+  buttonText: {
+    fontSize: 18,
+    fontWeight: '600',
+    color: '#fff',
+  },
+  businessList: {
+    paddingVertical: 20,
+  },
+  businessItem: {
+    backgroundColor: '#fff',
+    padding: 20,
+    borderRadius: 10,
+    marginBottom: 10,
+    elevation: 2,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+  },
+  businessName: {
+    fontSize: 18,
+    fontWeight: '700',
+    color: '#075eec',
+    marginBottom: 6,
+  },
+  businessDetails: {
+    fontSize: 14,
+    color: '#929292',
+  },
+  footer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    backgroundColor: '#fff',
+    paddingVertical: 10,
+    borderTopWidth: 1,
+    borderTopColor: '#e8ecf4',
+  },
+  footerIcon: {
+    alignItems: 'center',
+  },
+  footerIconText: {
+    fontSize: 12,
+    color: '#075eec',
+  },
+});
---
+++ splendrApp/App/Screens/ClientProfile.js
@@ -0,0 +1,194 @@
+// ClientProfile.js
+import React, { useState } from 'react';
+import { StyleSheet, SafeAreaView, View, Text, TouchableOpacity, ScrollView, Dimensions } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import LogoutConfirmation from './LogoutConfirmation'; // Import the modal component
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+
+const { width } = Dimensions.get('window');
+
+const ClientProfile = () => {
+  const navigation = useNavigation();
+  const [logoutModalVisible, setLogoutModalVisible] = useState(false);
+
+  const handleLogout = () => {
+    setLogoutModalVisible(true);
+  };
+
+  const handlePressProfile = () => {
+    navigation.navigate('BeauticianProfileScreen');
+  };
+
+  const handlePress = () => {
+    navigation.navigate('AppointmentScreen');
+  };
+
+  const handleSearchBusiness = () => {
+    navigation.navigate('SearchBusinessScreen');
+  };
+
+  const handleRatings = () => {
+    navigation.navigate('RatingsScreen');
+  };
+
+  const confirmLogout = () => {
+    // Perform logout logic here (clear user data, tokens, etc.)
+    // For example, navigate to Login screen after logout
+    navigation.navigate('Login');
+  };
+
+  const cancelLogout = () => {
+    setLogoutModalVisible(false);
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.title}> Profile </Text>
+      </View>
+
+      <ScrollView contentContainerStyle={styles.scrollContainer}>
+        <View style={styles.buttonContainer}>
+         
+
+          <TouchableOpacity style={styles.boxButton} onPress={handlePressProfile}>
+            {/* <Image source={require('./assets/profile.png')} style={styles.buttonImage} /> */}
+            <Text style={styles.boxButtonText}>View Profile</Text>
+          </TouchableOpacity>
+
+         
+
+          <TouchableOpacity style={styles.boxButton} onPress={handleRatings}>
+            {/* <Image source={require('./assets/ratings.png')} style={styles.buttonImage} /> */}
+            <Text style={styles.boxButtonText}>Ratings</Text>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+
+      <View style={styles.content}>
+        <TouchableOpacity style={styles.button} onPress={handleLogout}>
+          <Text style={styles.buttonText}>Logout</Text>
+        </TouchableOpacity>
+
+        <LogoutConfirmation
+          visible={logoutModalVisible}
+          onConfirm={confirmLogout}
+          onCancel={cancelLogout}
+        />
+      </View>
+
+      <View style={styles.footer}>
+        <TouchableOpacity style={styles.footerIcon} onPress={() => navigation.navigate('ClientHomeScreen')}>
+          <Icon name="home" size={24} color="#075eec" />
+          <Text style={styles.footerIconText}>Home</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.footerIcon} onPress={() => navigation.navigate('AppointmentBookingScreen')}>
+          <Icon name="calendar" size={24} color="#075eec" />
+          <Text style={styles.footerIconText}>Appointments</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.footerIcon} onPress={() => navigation.navigate('ClientProfile')}>
+          <Icon name="user" size={24} color="#075eec" />
+          <Text style={styles.footerIconText}>Profile</Text>
+        </TouchableOpacity>
+      </View>
+
+    </SafeAreaView>
+  );
+};
+
+export default ClientProfile;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#e8ecf4',
+  },
+  header: {
+    alignItems: 'center',
+    marginVertical: 36,
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: '700',
+    color: '#075eec',
+    marginBottom: 6,
+  },
+  subtitle: {
+    fontSize: 18,
+    fontWeight: '500',
+    color: '#929292',
+  },
+  scrollContainer: {
+    flexGrow: 1,
+    paddingHorizontal: 24,
+    paddingBottom: 20,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+  },
+  boxButton: {
+    width: width * 0.85, // Adjusted width to fit the screen
+    height: width * 0.20, // Make the height equal to the width for square buttons
+    backgroundColor: '#fff',
+    padding: 10,
+    borderRadius: 10,
+    marginVertical: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row', // Added flexDirection to arrange content horizontally
+    elevation: 2, // Add shadow for Android
+    shadowColor: '#000', // Add shadow for iOS
+    shadowOffset: { width: 0, height: 2 }, // Add shadow for iOS
+    shadowOpacity: 0.25, // Add shadow for iOS
+    shadowRadius: 3.84, // Add shadow for iOS
+  },
+  buttonImage: {
+    width: 50,
+    height: 50,
+    marginRight: 10, // Added margin for image and text separation
+  },
+  boxButtonText: {
+    fontSize: 16,
+    fontWeight: '600',
+    color: '#1D2A32',
+    textAlign: 'center',
+    flex: 1, // Allow text to take remaining space
+  },
+  content: {
+    alignItems: 'center',
+    paddingBottom: 20,
+  },
+  button: {
+    backgroundColor: '#075eec',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 25,
+    marginVertical: 10,
+  },
+  buttonText: {
+    fontSize: 18,
+    fontWeight: '600',
+    color: '#fff',
+  },
+
+  footer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    backgroundColor: '#fff',
+    paddingVertical: 10,
+    borderTopWidth: 1,
+    borderTopColor: '#e8ecf4',
+  },
+  footerIcon: {
+    alignItems: 'center',
+  },
+  footerIconText: {
+    fontSize: 12,
+    color: '#075eec',
+  },
+
+
+});
